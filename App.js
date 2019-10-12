1a77e930fda712dccf5722d1b3aeccade40de340
Jerry Chen <54541460+jerrychen-2018@users.noreply.github.com>
2019-10-12 04:23:17 -0400

    Basic React Form

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import './App.css';
+import 'typeface-roboto';
+import Form from "./Form";
+
+class App extends Component{
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    })
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <Form onChange={fields => this.onChange(fields)}/>
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+        </p>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
